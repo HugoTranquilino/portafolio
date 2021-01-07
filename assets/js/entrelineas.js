3ec93a11be6309cb42e1f9e409510486d5a08f59
Hugo <hugotranquilino@gmail.com>
2021-01-07 17:38:27 -0600

    cambio general al frontend

--- assets/js/entrelineas.js
+++ assets/js/entrelineas.js
@@ -9,15 +9,50 @@ $(document).ready(function () {
         $('#secound-item').show();
         $('#first-item').hide();
         $('#list-trabajos').css('display', 'block');
-        mostrarTrabajos();
+        // mostrarTrabajos();
     });
 
     function mostrarTrabajos() {
         const contenedor = document.querySelector('#list-trabajos');
         let html = `<div class="col-md-12">
                         <center>
-                            <h1 class="title">Trabajos</h1>
+                            <h1 class="title">Experiencia Laboral</h1>
                         </center>
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-3 card-imagehover">
+                                    <figure class="imagehover imghvr-zoom-out-flip-horiz">
+                                        <img src="assets/imagenes/contraloria.png">
+                                        <figcaption>
+                                          // Hover Content
+                                        </figcaption>
+                                        <a href="#"></a>
+                                    </figure>
+                                </div>
+                                <div class="col-md-3 card-imagehover">
+                                    <figure class="imagehover imghvr-fold-left">
+                                        <img src="assets/imagenes/onbit.png">
+                                        <figcaption>
+                                          // Hover Content
+                                        </figcaption>
+                                        <a href="#"></a>
+                                    </figure>
+                                </div>
+                                <div class="col-md-3 card-imagehover">
+                                    <figure class="imagehover imghvr-hinge-right">
+                                        <img src="assets/imagenes/tec.jpg">
+                                        <figcaption>
+                                            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+                                                deleniti, magnam neque fugiat dolore, nesciunt doloribus ab culpa
+                                                blanditiis explicabo, sed ipsa vel obcaecati minima repellat delectus
+                                                nulla nam inventore.</span>
+                                        </figcaption>
+                                        <a href="#"></a>
+                                    </figure>
+                                </div> 
+                            </div>
+                        </div>
+
                         <div class="col-md-12">
                             <div class="row">
                                 <div class="col-md-6">
